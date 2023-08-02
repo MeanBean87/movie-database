16a9f5c26aa4aa128704272e01df80f1c9d05cf2
Michael Mattingly <mikeamatt1@hotmail.com>
2023-08-02 16:42:14 -0400

    Created movie and review seeds, and verified DB creation.

--- db/seeds.js
+++ db/seeds.js
@@ -1,18 +1,69 @@
-const seedDepartments = async (pool) => {
-    const departments = [
-      { name: "Sales" },
-      { name: "Engineering" },
-      { name: "Finance" },
-      { name: "Legal" },
-      { name: "Human Resources" },
-      { name: "Marketing" },
-      { name: "Customer Service" },
-      { name: "Research and Development" },
-      { name: "Purchasing" },
-      { name: "Information Technology" },
+const seedMovies = async (pool) => {
+    const movies = [
+        { movie_name: "E.T." },
+        { movie_name: "The Goonies" },
+        { movie_name: "Back to the Future" },
+        { movie_name: "The Breakfast Club" },
+        { movie_name: "Ferris Bueller's Day Off" },
+        { movie_name: "Top Gun" },
+        { movie_name: "The Princess Bride" },
+        { movie_name: "Dirty Dancing" },
+        { movie_name: "The Karate Kid" },
+        { movie_name: "Ghostbusters" }
     ];
   
-    await pool.query(`INSERT INTO departments (name) VALUES ?`, [
-      departments.map((dep) => [dep.name]),
+    await pool.query(`INSERT INTO movies (movie_name) VALUES ?`, [
+      movies.map((movie) => [movie.movie_name]),
     ]);
-  };
+};
+
+const seedReviews = async (pool) => {
+    const reviews = [
+        {
+            movie_id: 10,
+            review: "This movie is great!"
+        },
+        {
+            movie_id: 9,
+            review: "This movie is terrible!"
+        },
+        {
+            movie_id: 8,
+            review: "This movie is awesome!"
+        },
+        {
+            movie_id: 7,
+            review: "This movie is awful!"
+        },
+        {
+            movie_id: 6,
+            review: "This movie is amazing!"
+        },
+        {
+            movie_id: 5,
+            review: "This movie is horrible!"
+        },
+        {
+            movie_id: 4,
+            review: "This movie is fantastic!"
+        },
+        {
+            movie_id: 3,
+            review: "This movie is terrible!"
+        },
+        {
+            movie_id: 2,
+            review: "This movie is great!"
+        },
+        {
+            movie_id: 1,
+            review: "This movie is awful!"
+        }
+    ];
+
+    await pool.query(`INSERT INTO reviews (movie_id, review) VALUES ?`, [
+        reviews.map((review) => [review.movie_id, review.review]),
+      ]);
+}
+
+module.exports = {seedMovies, seedReviews};
